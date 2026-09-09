7695e684b4514ff3f0bef3ff48bfba4dc3514b83
Tadios <tadiosmisganaw@gmail.com>
2026-09-09 14:37:48 +0300

    fix menu page load problem

--- client/src/pages/customer/MenuPage.jsx
+++ client/src/pages/customer/MenuPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { LanguageContext } from "../../context/LanguageContext";
